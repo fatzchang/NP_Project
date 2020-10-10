f820d53c13a8bc3ea2acbd80fe2f76178fe09b1b
ubuntu <ubuntu@ubuntu-Z370M-D3H.isip.cs.nctu.edu.tw>
2020-10-10 12:40:20 +0800

    parse input string

--- npshell.cpp
+++ npshell.cpp
@@ -1,31 +1,77 @@
 #include <string>
 #include <iostream>
+#include <regex>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 using namespace std;
 
 string PATH = "bin/";
+regex r("\\s(\\||!)");
 
 int main() {
     string input;
     string cmd;
+    int pipefd[2];
+    bool isFirst = true;
 
     while(1) {
         cout << "% ";
-        cin >> input;
-        cmd = input.substr(0, input.find(" "));
-        cout << cmd << endl;
-        pid_t pid;
+        getline(cin, input);
+        smatch s;
 
-        if (pid = fork() < 0) {
-            cout << "fork err" << endl;
-            exit(0);
-        } else {
-            wait();
-            cout << pid << endl;
-            exit(0);
+        while (regex_search(input, s, r))
+        {
+            cmd = input.substr(0, s.position());
+            cout << "command: " << cmd << endl;
+            input = input.substr(s.position() + (s.size() + 1));
         }
+
+        // last command
+        cout << "command: " << input << endl;
+        
+        // if (regex_search(input, s, r)) {
+        //     cout << s.position() << endl;
+        //     // for (int i = 0; i < s.size(); i++) {
+        //     //     cout << i << ": " << s[i] << endl;
+        //     // }
+        // } else {
+        //     cout << "not matched" << endl; 
+        // }
+
+        // not built-in command
+        // while (input.find("|") != 0) {
+        //     if (!isFirst) {
+        //         // if not first command, close stdin
+        //         close(STDIN_FILENO);
+        //         dup(pipefd[0]);
+        //     }
+        //     close(STDOUT_FILENO);
+        //     dup(pipefd[1]);
+        //     isFirst = false;
+
+
+        //     if (pipe(pipefd) < 0) {
+        //     // error
+        //         cout << "pipe err" << endl;
+        //     } else {
+
+        //     }
+        // }
+        
+        // pid_t pid = fork();
+        // if (pid  < 0) {
+        //     cout << "fork err" << endl;
+        // } else {
+        //     if (pid == 0) {
+        //         // child process
+        //         execl((PATH + cmd).c_str(), "-l", NULL);
+        //     } else {
+        //         // parent process
+        //         waitpid(pid, NULL, WNOHANG); // not block waiting
+        //     }
+        // }
     }
     return 0;
 }
