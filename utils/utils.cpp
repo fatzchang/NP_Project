611abc69553769db6e9cc01cf47850ee9b32f20c
fatz_chang <fatz.zhang@gmail.com>
2020-10-20 17:21:08 +0800

    fix read buffer

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -37,13 +37,13 @@ void output(
 ) {
     ofstream my_file(filename);
     char buf[BUF_SIZE + 1];
-    // memset(buf, '\0', BUF_SIZE + 1);
-    // read(prev_pipe_read, buf, BUF_SIZE);
-    while (int num_read = read(prev_pipe_read, buf, BUF_SIZE) > 0) {
+    memset(buf, '\0', BUF_SIZE + 1);
+
+    int num_read;
+    while ((num_read = read(prev_pipe_read, buf, BUF_SIZE)) > 0) {
         buf[num_read] = '\0';
         my_file << buf;    
     }
-    // my_file << buf;
     my_file.close();
 }
 
@@ -92,18 +92,13 @@ int pipe_worker(vector<map<string, int>> &num_pipe_list) {
     for (size_t i = 0; i < num_pipe_list.size(); i++) {
         if (num_pipe_list.at(i).find("counter")->second == 0) {
             char buf[BUF_SIZE + 1];
-            // memset(buf, '\0', BUF_SIZE + 1);
-            // read(num_pipe_list.at(i).find("read")->second, buf, BUF_SIZE);
-
-            while (int num_read = read(num_pipe_list.at(i).find("read")->second, buf, BUF_SIZE) > 0) {
+            int num_read;
+            while ((num_read = read(num_pipe_list.at(i).find("read")->second, buf, BUF_SIZE)) > 0) {
                 buf[num_read] = '\0';
                 cout << buf;
             }
-
-            cout << buf;
         }
     }
-
     cout << flush;
     replace_fd(STDOUT_FILENO, stdout_tmp);
     erase_num_pipe(num_pipe_list);
